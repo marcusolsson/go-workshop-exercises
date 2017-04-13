b4b0963216990ea9f993b13701f0598a82aab214
Marcus Olsson <ninjasinventedphysics@gmail.com>
2017-04-13 18:32:02 +0200

    Add potion exercise

---
+++ potion/potion.go
@@ -0,0 +1,20 @@
+package potion
+
+import "fmt"
+
+// STRUCT GOES HERE
+
+func main() {
+	p := player{
+		nickname: "Anonymous",
+		health:   12,
+	}
+
+	p.consumePotion()
+
+	if p.health == 100 {
+		fmt.Println(p.nickname, "lives to fight another day")
+	} else {
+		fmt.Println(p.nickname, "still suffers")
+	}
+}
